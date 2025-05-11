af87af1dc6bba6c6b20b575c8c49f881b8735f22
Kausthubh J Rao <105716675+Exgene@users.noreply.github.com>
2025-05-11 20:58:16 +0530

    feat: async context manager

--- main.py
+++ main.py
@@ -1,22 +1,95 @@
+import asyncio
 import os
+from pathlib import Path
+from typing import Optional
 
 import dotenv
 from langchain_groq import ChatGroq
+from playwright.async_api import (
+    BrowserContext,
+    Page,
+    Playwright,
+    ProxySettings,
+    async_playwright,
+)
 
 dotenv.load_dotenv(".env")
 
 if "GROQ_API_KEY" not in os.environ:
     assert "MISSING API KEYS"
 
+proxies: list[ProxySettings] = [
+    {"server": "", "username": "", "password": ""},
+    {"server": "", "username": "", "password": ""},
+]
 
-def main():
+
+class PlaywrightInstance:
+    def __init__(self, headless: bool = True) -> None:
+        self.playwright: Optional[Playwright]
+        self.browser: Optional[BrowserContext]
+        self.user_data_dir: Path = Path("./user_data_dir")
+        self.page: Optional[Page]
+        self.headless = headless
+
+    async def __aenter__(self):
+        await self.create_playwright_instance()
+        return self
+
+    async def __aexit__(self, exc_t, exc_v, exc_tb):
+        print("Cleaning up resources...")
+        print(self.page)
+        print(self.browser)
+        try:
+            if self.page:
+                await self.page.close()
+                print("Closed Page")
+                self.page = None
+
+            if self.browser:
+                await self.browser.close()
+                print("closed browser")
+                self.browser = None
+
+            if self.playwright:
+                await self.playwright.stop()
+                print("stopped playwright")
+                self.playwright = None
+
+        except Exception as e:
+            print(f"Error during cleanup: {e}")
+
+        return False
+
+    async def create_playwright_instance(self):
+        self.playwright = await async_playwright().start()
+        proxy = proxies[0]
+
+        chromium = self.playwright.chromium
+        print(self.user_data_dir)
+        self.browser = await chromium.launch_persistent_context(
+            user_data_dir="./user_data_dir",
+            headless=self.headless,
+        )
+
+        self.page = await self.browser.new_page()
+
+    async def run(self):
+        assert self.browser, "INTITIALZE THE PLAYWRIGHT INSTANCE"
+        await self.page.goto("https://kausthubh.com")
+        await asyncio.sleep(4)
+
+
+async def main():
     llm = ChatGroq(
         model="llama-3.1-8b-instant",
     )
 
     res = llm.invoke(input="hello")
     print(res)
+    async with PlaywrightInstance(headless=False) as p:
+        await p.run()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
